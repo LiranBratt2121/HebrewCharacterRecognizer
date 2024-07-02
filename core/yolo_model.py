b0c53b31b2b49610492527c0c22cfafdaa99b451
LiranBratt2121 <brattlirannin@gmail.com>
2024-07-03 02:48:09 +0300

    Add basic YOLO wrapper

---
+++ core/yolo_model.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+class YoloModel:
+    def __init__(self, model_weights="weights/best.pt") -> None:
+        self.model = YOLO(model_weights)
+
+    def predict_class(self, image_path: str, conf=0.2) -> str:
+        result = self.model.predict(source=image_path, conf=conf, verbose=False)[0]
+        class_name = result.names.get(result.boxes.cls[0].item(), 'Unknown')
+        return class_name
