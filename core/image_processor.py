47c8d8959d37908f86dd08eccb7b56d74f7841ec
LiranBratt2121 <brattlirannin@gmail.com>
2024-08-01 19:09:10 +0300

    Update sizing

--- core/image_processor.py
+++ core/image_processor.py
@@ -11,11 +11,11 @@ class ImageProcessor:
         data = image.getdata()
         updated_image_data = [(0, 0, 0, 255) if item[3] > 0 else item for item in data]
         image.putdata(updated_image_data)
-        return image.resize((800, 600))
+        return image.resize((640, 480))
 
     def __add_white_background(self, image: Image.Image) -> Image.Image:
-        white_background = Image.new("RGBA", list(map(lambda s: s * 2, image.size)), (255, 255, 255, 255))
-        white_background.paste(image, list(map(lambda s: s // 2, image.size)), image)
+        white_background = Image.new("RGBA", list(map(lambda s: s * 5, image.size)), (255, 255, 255, 255))
+        white_background.paste(image, list(map(lambda s: s // 5, image.size)), image)
         return white_background
 
     def __save_image(self, image: Image.Image, output_path: str) -> None:
